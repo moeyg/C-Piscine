58d442f334fdd7e044654eabec42010f106c2cb0
moeyg <moooeyg@gmail.com>
2022-10-03 13:20:00 +0900

    C02 ex08

---
+++ Piscine/C/C02/ex08/ft_strlowcase.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 13:46:47 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/04 13:50:23 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (str[index] >= 'A' && str[index] <= 'Z')
+		{
+			str[index] = str[index] + 32;
+		}
+		index ++;
+	}
+	return (str);
+}
