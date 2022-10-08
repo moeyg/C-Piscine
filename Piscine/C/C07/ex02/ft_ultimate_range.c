3c35625ffc0007eab2018fb4e30f4c6abf6022b4
moeyg <moooeyg@gmail.com>
2022-10-08 21:36:30 +0900

    C07 ex02

---
+++ Piscine/C/C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 01:08:24 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/19 16:55:14 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int		*array;
+	long long	index;
+
+	if (min >= max)
+		return (0);
+	array = (int *)malloc(sizeof(int) * (max - min));
+	if (array == 0)
+		return (0);
+	index = 0;
+	while (index < max - min)
+	{
+		array[index] = min + index;
+		index ++;
+	}
+	*range = array;
+	return (max - min);
+}
