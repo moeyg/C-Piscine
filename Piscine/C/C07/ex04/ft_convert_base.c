bb16c2de009e2cfea368e069aec759e42494eecf
moeyg <moooeyg@gmail.com>
2022-10-08 22:26:33 +0900

    C07 ex04

---
+++ Piscine/C/C07/ex04/ft_convert_base.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 13:14:42 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/19 15:41:05 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_atoi_base(char *nbr, char *base, int radix);
+int	ft_check_base(char *base);
+int	ft_divide_integers_by_base(int integer, int radix);
+char	*ft_convert_number(int integer, int radix, char *base);
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	integer;
+	int	radix_to;
+	int	radix_from;
+
+	radix_from = ft_check_base(base_from);
+	radix_to = ft_check_base(base_to);
+	if (radix_from < 2 || radix_to < 2)
+		return (0);
+	integer = ft_atoi_base(nbr, base_from, radix_from);
+	return (ft_convert_number(integer, radix_to, base_to));
+}
+
+int	ft_check_base(char *base)
+{
+	char	*cmp;
+	int	index;
+
+	index = 0;
+	while (base[index])
+	{
+		cmp = &base[index];
+		if (base[index] == '+' || base[index] == '-'
+			|| base[index] == '\t' || base[index] == '\n' || base[index] == '\v'
+			|| base[index] == '\f' || base[index] == '\r' || base[index] == ' ')
+			return (0);
+		while (*(cmp++))
+		{
+			if (*cmp == base[index])
+				return (0);
+		}
+		index ++;
+	}
+	return (index);
+}
+
+char	*ft_convert_number(int integer, int radix, char *base)
+{
+	int	size;
+	int	index;
+	char	*numbers;
+
+	size = ft_divide_integers_by_base(integer, radix);
+	numbers = (char *)malloc((sizeof(char) * size + 1));
+	if (!numbers)
+		return (0);
+	numbers[size] = '\0';
+	if (integer < 0)
+	{
+		numbers[0] = '-';
+		numbers[--size] = base[-(integer % radix)];
+		integer = -(integer / radix);
+		index = 1;
+	}
+	else
+		index = 0;
+	while (index < size)
+	{
+		numbers[--size] = base[integer % radix];
+		integer /= radix;
+	}
+	return (numbers);
+}
+
+int	ft_divide_integers_by_base(int integer, int radix)
+{
+	int	length;
+
+	length = 1;
+	while (integer / radix != 0)
+	{
+		if (integer < 0)
+		{
+			integer = -(integer / radix);
+			length += 2;
+		}
+		else
+		{
+			integer = integer / radix;
+			length ++;
+		}
+	}
+	return (length);
+}
---
+++ Piscine/C/C07/ex04/ft_convert_base2.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 13:10:31 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/19 15:39:05 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_check_number(char nbr, char *base);
+
+int	ft_atoi_base(char *nbr, char *base, int radix)
+{
+	int		sign;
+	int		number;
+	int		result;
+
+	if (radix < 2)
+		return (0);
+	sign = 1;
+	result = 0;
+	while (*nbr == '\t' || *nbr == '\n' || *nbr == '\v'
+		|| *nbr == '\f' || *nbr == '\r' || *nbr == ' ')
+		nbr ++;
+	while (*nbr == '+' || *nbr == '-')
+	{
+		if (*(nbr)++ == '-')
+			sign = -sign;
+	}
+	while (*nbr)
+	{
+		number = ft_check_number(*nbr, base);
+		if (number < 0)
+			break ;
+		result = (result * radix) + (sign * number);
+		nbr ++;
+	}
+	return (result);
+}
+
+int	ft_check_number(char nbr, char *base)
+{
+	int	index;
+
+	index = 0;
+	while (base[index])
+	{
+		if (nbr == base[index])
+			return (index);
+		index ++;
+	}
+	return (-1);
+}
