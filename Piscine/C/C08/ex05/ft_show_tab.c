18eb115e3fad115330dba3594de3e0ce537560cc
moeyg <moooeyg@gmail.com>
2022-10-10 04:28:12 +0900

    C08 ex05

---
+++ Piscine/C/C08/ex05/ft_show_tab.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 18:05:25 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/17 18:07:59 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putchar(char str);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	index;
+
+	index = 0;
+	while (par[index].str != 0)
+	{
+		ft_putstr(par[index].str);
+		ft_putchar('\n');
+		ft_putnbr(par[index].size);
+		ft_putchar('\n');
+		ft_putstr(par[index].copy);
+		ft_putchar('\n');
+		index ++;
+	}
+}
+
+void	ft_putchar(char nb)
+{
+	write(1, &nb, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index ++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
