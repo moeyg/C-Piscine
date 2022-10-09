f04153175d3289e85539bb12bc9330dae44ab6d1
moeyg <moooeyg@gmail.com>
2022-10-10 04:02:00 +0900

    C08 ex04

---
+++ Piscine/C/C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:46:45 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/17 19:45:30 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+char	*ft_strdup(char *src);
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int		index;
+	t_stock_str	*tab;
+
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (tab == 0)
+		return (0);
+	index = 0;
+	while (index < ac)
+	{
+		tab[index].size = ft_strlen(av[index]);
+		tab[index].str = av[index];
+		tab[index].copy = ft_strdup(av[index]);
+		index ++;
+	}
+	tab[index].str = 0;
+	return (tab);
+}
+
+int	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != 0)
+	{
+		index ++;
+	}
+	return (index);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int	index;
+	int	size;
+
+	size = ft_strlen(src);
+	dest = malloc(sizeof(char) * (size + 1));
+	if (dest == 0)
+		return (0);
+	index = 0;
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index ++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
