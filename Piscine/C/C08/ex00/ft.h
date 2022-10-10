963295e751939d83a35c389e2fc10226947deab9
moeyg <moooeyg@gmail.com>
2022-10-10 21:23:00 +0900

    C08 ex00

---
+++ Piscine/C/C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:05:40 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/17 14:33:44 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int	ft_strlen(char *str);
+int	ft_strcmp(char *s1, char *s2);
+
+#endif
