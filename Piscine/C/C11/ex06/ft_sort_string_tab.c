b6665e439fe173db234e8649f336f951efa4665f
moeyg <moooeyg@gmail.com>
2022-10-12 15:08:05 +0900

    C11 ex06

---
+++ Piscine/C/C11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 10:23:01 by dogpark           #+#    #+#             */
+/*   Updated: 2022/10/19 15:00:53 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2);
+void	ft_swap(char **s1, char **s2);
+
+void	ft_sort_string_tab(char **tab)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (tab[i + 1] != 0)
+	{
+		j = i + 1;
+		while (tab[j] != 0)
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+				ft_swap(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	index;
+
+	index = 0;
+	while (s1[index] != '\0' || s2[index] != '\0')
+	{
+		if (s1[index] == s2[index])
+			index ++;
+		else
+			return (s1[index] - s2[index]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
