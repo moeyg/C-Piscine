f5cb8f11f89b4691a84af591273c796cf96ab205
moeyg <moooeyg@gmail.com>
2022-10-23 20:21:42 +0900

    C00 ex02

---
+++ Piscine/C/C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 16:22:06 by dogpark           #+#    #+#             */
+/*   Updated: 2022/09/29 16:36:54 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		write(1, &alphabet, 1);
+		alphabet--;
+	}
+}
